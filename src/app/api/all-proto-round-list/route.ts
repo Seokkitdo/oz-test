787013c87a10393d2ad91e2e3815dbc433e6e3c7
Seokkitdo <sjd12329@gmail.com>
2024-03-23 15:10:46 +0900

    에러핸들링 추가

--- src/app/api/all-proto-round-list/route.ts
+++ src/app/api/all-proto-round-list/route.ts
@@ -11,16 +11,20 @@ export async function GET(request: Request) {
     },
   };
 
-  const res = await axios(
-    "https://www.betman.co.kr/buyPsblGame/lotterySchedulesInq.do",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: requestBody,
-    }
-  );
-
-  return new Response(JSON.stringify(res.data));
+  try {
+    const res = await axios(
+      "https://www.betman.co.kr/buyPsblGame/lotterySchedulesInq.do",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: requestBody,
+      }
+    );
+    return new Response(JSON.stringify(res.data));
+  } catch (error) {
+    console.error(error);
+    // 에러에 따른 적절한 응답 반환
+  }
 }
