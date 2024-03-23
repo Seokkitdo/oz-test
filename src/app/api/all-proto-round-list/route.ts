d89fa6dc6c397f4b8fb898f59a0649f00f7a24f8
Seokkitdo <sjd12329@gmail.com>
2024-03-23 15:32:25 +0900

    axios 복구

--- src/app/api/all-proto-round-list/route.ts
+++ src/app/api/all-proto-round-list/route.ts
@@ -1,4 +1,5 @@
 import { TAllProtoRoundItem } from "@/app/_types/allProtoRoundList";
+import axios from "axios";
 
 export async function GET(request: Request) {
   const requestBody = {
@@ -11,22 +12,17 @@ export async function GET(request: Request) {
   };
 
   try {
-    const res = await fetch(
+    const res = await axios(
       "https://www.betman.co.kr/buyPsblGame/lotterySchedulesInq.do",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // "Access-Control-Allow-Origin": "*", // 이 헤더는 클라이언트에서 설정할 수 없습니다.
         },
-        body: JSON.stringify(requestBody), // fetch에서는 data 대신 body 키워드를 사용합니다.
+        data: requestBody,
       }
     );
-
-    if (!res.ok) throw new Error("Response was not OK.");
-
-    const data = await res.json();
-    return new Response(JSON.stringify(data));
+    return new Response(JSON.stringify(res.data));
   } catch (error) {
     console.error(error);
     // 에러에 따른 적절한 응답 반환
