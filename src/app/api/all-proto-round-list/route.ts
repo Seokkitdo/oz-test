54df5a67a6dd751fea0999ffc7a63a73447e83c0
Seokkitdo <sjd12329@gmail.com>
2024-03-23 15:25:19 +0900

    fetch 로 변경

--- src/app/api/all-proto-round-list/route.ts
+++ src/app/api/all-proto-round-list/route.ts
@@ -1,5 +1,4 @@
 import { TAllProtoRoundItem } from "@/app/_types/allProtoRoundList";
-import axios from "axios";
 
 export async function GET(request: Request) {
   const requestBody = {
@@ -12,18 +11,22 @@ export async function GET(request: Request) {
   };
 
   try {
-    const res = await axios(
+    const res = await fetch(
       "https://www.betman.co.kr/buyPsblGame/lotterySchedulesInq.do",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Origin": "*", // 이 헤더는 클라이언트에서 설정할 수 없습니다.
         },
-        data: requestBody,
+        body: JSON.stringify(requestBody), // fetch에서는 data 대신 body 키워드를 사용합니다.
       }
     );
-    return new Response(JSON.stringify(res.data));
+
+    if (!res.ok) throw new Error("Response was not OK.");
+
+    const data = await res.json();
+    return new Response(JSON.stringify(data));
   } catch (error) {
     console.error(error);
     // 에러에 따른 적절한 응답 반환
